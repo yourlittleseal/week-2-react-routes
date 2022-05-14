1e83c72066be7ca680e142c6c2c7acb3b60a85af
Анна <littleseal@MacBook-Air-Anna.local>
2022-05-14 17:50:18 +0200

    homework done

--- client/components/home.js
+++ client/components/home.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import Header from './header'
+import { Route } from 'react-router-dom'
+import Header from './header' 
+import Dashboard from './dashboard'
+import Main from './main'
+import Profile from './profile'
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          {/* add your routes here */}
+            <Route exact path="/dashboard/" component={() => <Dashboard />} />  
+            <Route exact path="/dashboard/main" component={() => <Main />} /> 
+            <Route exact path="/dashboard/profile/:username" component={() => <Profile />} /> 
         </div>
-      </div>
-    </div>
   )
 }
 
--- client/config/root.js
+++ client/config/root.js
@@ -84,7 +84,7 @@ const RootComponent = (props) => {
         <StartupConnected>
           <Switch>
             <Route exact path="/" component={() => <DummyView />} />
-            <Route exact path="/dashboard" component={() => <Home />} />
+            <Route exact path="/dashboard/*" component={() => <Home />} />
             <PrivateRouteConnected exact path="/hidden-route" component={() => <DummyView />} />
             <Route component={() => <NotFound />} />
           </Switch>
